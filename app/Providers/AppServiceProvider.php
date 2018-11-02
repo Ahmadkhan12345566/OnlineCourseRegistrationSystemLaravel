c67f880968057ccce03902b8e5b9e20d3608ba54
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2018-11-03 00:11:02 +0500

    https in servis providers in boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(config('app.env') === 'production') {
-            URL::forceScheme('https');
+            \URL::forceScheme('https');
         }
     }
 
