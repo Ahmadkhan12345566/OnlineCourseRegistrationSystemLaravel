4d47ef3ede3a5be04bd1fcfe0be09f8995fdff08
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2018-11-02 23:41:48 +0500

    remove https to app.php

--- config/app.php
+++ config/app.php
@@ -56,7 +56,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
